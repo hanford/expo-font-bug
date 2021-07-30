8ce41f337c8d0aaf9c0cf52688d3010e96ecadf7
Jack Hanford <jackhanford@gmail.com>
2021-07-29 18:48:06 -0700

    init

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import * as Font from "expo-font";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const fonts = {
+  babysweet: require("./assets/fonts/babysweet.ttf"),
+};
+
+export default class App extends Component {
+  state = {
+    loaded: false,
+  };
+
+  componentDidMount() {
+    this._loadAssetsAsync();
+  }
+
+  _loadAssetsAsync = async () => {
+    await Font.loadAsync(fonts);
+
+    this.setState({ loaded: true });
+  };
+
+  render() {
+    if (!this.state.loaded) {
+      return (
+        <View style={[styles.container, { flexDirection: "row" }]}>
+          <ActivityIndicator />
+          <Text style={{ marginLeft: 8, color: "black" }}>
+            Loading fonts...
+          </Text>
+        </View>
+      );
+    }
+
+    return (
+      <View style={styles.container}>
+        <Text style={{ fontFamily: "babysweet", color: "black" }}>
+          SF font, yo!
+        </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
